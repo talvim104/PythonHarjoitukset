2778db4a896d665bd7845f0583b077959f26d694
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-01-21 09:37:13 +0200

    New code

--- mod 02/Teht2/Teht6.py
+++ mod 02/Teht2/Teht6.py
@@ -1,12 +1,12 @@
 import random
-koodi = random.randint(100, 999)
-print("Kolmenumeroinen koodi:", koodi)
+num1 = random.randint(0, 9)
+num2 = random.randint(0, 9)
+num3 = random.randint(0, 9)
+num4 = random.randint(0, 9)
+print("Kolmenumeroinen koodi:" + str(num1) + str(num2) + str(num3) + str(num4))
 
 num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
 num4 = random.randint(1, 6)
 print("Neljänumeroinen koodi:" + str(num1) + str(num2) + str(num3) + str(num4))
-
-koodi_2 = random.sample("123456",4)
-print("Neljänumeroinen koodi:", koodi_2)
