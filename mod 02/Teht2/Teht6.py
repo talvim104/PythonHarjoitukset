8bb1f470f99208405df87d1bb94a1cec357592bc
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-01-21 09:38:27 +0200

    New code

--- mod 02/Teht2/Teht6.py
+++ mod 02/Teht2/Teht6.py
@@ -3,7 +3,7 @@ num1 = random.randint(0, 9)
 num2 = random.randint(0, 9)
 num3 = random.randint(0, 9)
 num4 = random.randint(0, 9)
-print("Kolmenumeroinen koodi:" + str(num1) + str(num2) + str(num3) + str(num4))
+print("Kolmenumeroinen koodi:" + str(num1) + str(num2) + str(num3))
 
 num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
