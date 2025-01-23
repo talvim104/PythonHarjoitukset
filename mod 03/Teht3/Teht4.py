8f14f8ea72e4d73043e32c19496c3f2ad9a9ba26
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-01-23 11:54:38 +0200

    Add file

--- mod 03/Teht3/Teht4.py
+++ mod 03/Teht3/Teht4.py
@@ -1,7 +1,10 @@
 vuosi = int(input("Anna vuosiluku: "))
-if vuosi % 4 == 0:
+if vuosi % 400 == 0:
     print("Vuosi on karkausvuosi!")
-elif vuosi % 400 == 0 and vuosi % 100 == 0:
+elif vuosi % 100 == 0:
+    print("Vuosi ei ole karkausvuosi!")
+elif vuosi % 4 == 0:
     print("Vuosi on karkausvuosi!")
 else:
     print("Vuosi ei ole karkausvuosi!")
+
