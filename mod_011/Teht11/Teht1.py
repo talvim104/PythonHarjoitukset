ffced8384c432bfaab2bb63691a406b4b4ab3af5
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-03-26 14:48:39 +0200

    Add file

---
+++ mod_011/Teht11/Teht1.py
@@ -0,0 +1,28 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+class Kirja(Julkaisu):
+    def __init__(self,nimi, kirjoittaja, sivumaara):
+        self.kirjoittaja = kirjoittaja
+        self.sivumaara = sivumaara
+        Julkaisu.__init__(self,nimi)
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi,  paatoimittaja):
+        self. paatoimittaja =  paatoimittaja
+        Julkaisu.__init__(self, nimi)
+
+def tulostaa_tiedot(self):
+    print(f" Kirjan nimi on {my_book.nimi}, kirjoittaja on {my_book.kirjoittaja}, sivumäärä on {my_book.sivumaara} sivua.")
+
+my_book = Kirja("Hytti No6", "Rosa Liksom", 200)
+my_magazine = Lehti("Aku Ankka", "Aki Hyyppä")
+
+#print(f"Lehden nimi on {my_magazine.nimi} ja päätoimittaja on {my_magazine.paatoimittaja}")
+
+
+
+
+
+
